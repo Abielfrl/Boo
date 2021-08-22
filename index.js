0f929aafdd2bd16c4ce1abb599ce24417de21e30
Tudo12 <75420510+Tudo12@users.noreply.github.com>
2021-08-22 11:38:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client({
+  messageCacheLifetime: 60,
+  fetchAllMembers: false,
+  messageCacheMaxSize: 10,
+  restTimeOffset: 0,
+  restWsBridgetimeout: 100,
+  disableEveryone: true,
+  partials: ['MESSAGE', 'CHANNEL', 'REACTION']
+});
+
+
+//config
+const config = require("./config/config.json");
+const prefix = config.prefix;
+const token = config.token;
+
+// Message events
+client.on('message', message => {
+  if (message.content.startsWith("!ping")) {
+    message.channel.send(`${client.ws.ping} Pong!`);
+  }
+});
+
+
+
+
